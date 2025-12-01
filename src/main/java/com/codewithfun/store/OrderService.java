98fe8f98568fb52accccba3e0abb022d30075ad6
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2025-12-01 01:45:48 +0100

    Configuring Beans using annotation

--- src/main/java/com/codewithfun/store/OrderService.java
+++ src/main/java/com/codewithfun/store/OrderService.java
@@ -1,9 +1,16 @@
 package com.codewithfun.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderService {
     public PaymentService paymentService;
 
+    //when we have multiple construction we must use this annotation
+    // otherwise there will be an error. But as long as the class
+    // has only one constructor this annotation is optional
+    //@Autowired
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
--- src/main/java/com/codewithfun/store/StoreApplication.java
+++ src/main/java/com/codewithfun/store/StoreApplication.java
@@ -2,18 +2,19 @@ package com.codewithfun.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(StoreApplication.class, args);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-        var orderService = new OrderService(new StripePaymentService());
+        var orderService = context.getBean(OrderService.class);
         orderService.placeOrder();
 
-        orderService = new OrderService(new PaypalPaymentService());
-        orderService.placeOrder();
+//        orderService = new OrderService(new PaypalPaymentService());
+//        orderService.placeOrder();
 
     }
 
