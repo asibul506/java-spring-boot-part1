a99dc5964f9ca8ac81e0de177b8a5bd4eae11f01
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2025-12-01 23:31:48 +0100

    modification on the previous commit - Configuring beans using code

--- src/main/java/com/codewithfun/store/OrderService.java
+++ src/main/java/com/codewithfun/store/OrderService.java
@@ -1,13 +1,12 @@
 package com.codewithfun.store;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 //@Service
 public class OrderService {
     public PaymentService paymentService;
 
-    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
