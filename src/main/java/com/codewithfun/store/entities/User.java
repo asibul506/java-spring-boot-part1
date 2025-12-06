8c7e9f933fa7d1d80a867307ef90d476bbf07524
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2025-12-06 04:43:08 +0100

    Simplifying code with Lombok

--- src/main/java/com/codewithfun/store/entities/User.java
+++ src/main/java/com/codewithfun/store/entities/User.java
@@ -1,7 +1,14 @@
 package com.codewithfun.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter // This annotation is from Lombok library and it generates setter methods for all fields in the class
+@Getter // This annotation is from Lombok library and it generates getter methods for all fields in the class
+@AllArgsConstructor // This annotation is from Lombok library and it generates a constructor with parameters for all fields in the class. to use this annotation we need a NoArgsConstructor annotation as well or a default constructor
+@NoArgsConstructor // This annotation is from Lombok library and it generates a no-argument constructor
+@Builder // This annotation is from Lombok library and it helps to create object when some of the fields are optional unlike regular constructor where you have to pass all the fields
+@ToString // This annotation is from Lombok library and it generates a toString() method that includes all fields in the class
 @Entity // This annotation specifies that the class is an entity and is mapped to a database table
 @Table(name = "users") // This annotation specifies the name of the database table to be used for mapping
 public class User {
@@ -10,7 +17,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)// This annotation provides for the specification of generation strategies for the values of primary keys
     private Long id;
 
-/*  This (Column) annotation is used to specify the mapped column for a persistent property or field. If no Column annotation is specified, the default values
+/*
+    This (Column) annotation is used to specify the mapped column for a persistent property or field. If no Column annotation is specified, the default values
     are applied. Additionally, we are specifying the column names in the database table. if the column name is the same as the field name, this annotation is
     optional. otherwise, it is necessary. because it helps to map the field to the correct column in the database table.
 */
@@ -23,35 +31,4 @@ public class User {
     @Column(nullable = false, name = "password")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
